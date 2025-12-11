76140014c9a13d8a388da65be45846f226c7b464
Riski Pratama Nofianto <riskipn028@gmail.com>
2025-12-11 13:29:24 +0700

    Memperbaiki error

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 pages = [
-    st.page(page="pages/page1.py", title="Home", icon="🏠"),
-    st.page(page="pages/page2.py", title="visualisasi Data", icon="📈"),
-    st.page(page="pages/page3.py", title="Settings", icon="⚙️")
+    st.Page(page="pages/page1.py", title="Home", icon="🏠"),
+    st.Page(page="pages/page2.py", title="visualisasi Data", icon="📈"),
+    st.Page(page="pages/page3.py", title="Settings", icon="⚙️")
 ]
 
 pg = st.navigation(
